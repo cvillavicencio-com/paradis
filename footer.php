a9a439b1b1f6e9eb5302d12644cdce99848fecab
Camilo Villavicencio <laboral@cvillavicencio.com>
2023-03-06 15:40:42 -0300

    arreglo en textos, mapa en footer

--- footer.php
+++ footer.php
@@ -1,66 +1,64 @@
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-white py-5 px-sm-3 px-lg-5" style="margin-top: 90px;">
-        <div class="row pt-5">
-            <div class="col-12 mb-4 px-4">
-                <div class="row mb-5 p-4" style="background: rgba(256, 256, 256, .05);">
-                    <div class="col-md-4">
-                        <div class="text-md-center">
-                            <h5 class="text-primary text-uppercase mb-2" style="letter-spacing: 5px;">Nuestro taller</h5>
-         <p class="mb-4 m-md-0">Nueva San Martín 1096, Maipú</p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="text-md-center">
-                            <h5 class="text-primary text-uppercase mb-2" style="letter-spacing: 5px;">Email Us</h5>
-                            <p class="mb-4 m-md-0">info@example.com</p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="text-md-center">
-                            <h5 class="text-primary text-uppercase mb-2" style="letter-spacing: 5px;">Llámanos</h5>
-                            <p class="m-0">+569 5969 7897</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- <div class="col-lg-7 col-md-12">
-                 <div class="row">
-                 <div class="col-md-6 mb-5">
-                 <p>Et et amet ut elitr ipsum sit dolor dolor dolore. Sit accusam elitr ut diam sit rebum. Eirmod duo kasd diam vero labore sea, est et et lorem ut at erat, gubergren ipsum et ipsum elitr et rebum rebum</p>
-                 <div class="d-flex justify-content-start mt-4">
-                 <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
-                 <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
-                 <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
-                 <a class="btn btn-lg btn-outline-light btn-lg-square" href="#"><i class="fab fa-instagram"></i></a>
-                 </div>
-                 </div>
-                 <div class="col-md-6 mb-5">
-                 <h5 class="text-primary text-uppercase mb-4" style="letter-spacing: 5px;">Our Products</h5>
-                 <div class="d-flex flex-column justify-content-start">
-                 <a class="text-white btn-scroll mb-2" href="#service"><i class="fa fa-angle-right mr-2"></i>Solar System</a>
-                 <a class="text-white btn-scroll mb-2" href="#service"><i class="fa fa-angle-right mr-2"></i>Wind Turbine</a>
-                 <a class="text-white btn-scroll mb-2" href="#service"><i class="fa fa-angle-right mr-2"></i>Wind Generator</a>
-                 <a class="text-white btn-scroll mb-2" href="#service"><i class="fa fa-angle-right mr-2"></i>Solar Energy</a>
-                 <a class="text-white btn-scroll" href="#service"><i class="fa fa-angle-right mr-2"></i>Solar Panel</a>
-                 </div>
-                 </div>
-                 </div>
-								 </div>
-								 <div class="col-lg-5 col-md-12 mb-5">
-                 <h5 class="text-primary text-uppercase mb-4" style="letter-spacing: 5px;">Newsletter</h5>
-                 <p>Rebum labore lorem dolores kasd est, et ipsum amet et at kasd, ipsum sea tempor magna tempor. Accu kasd sed ea duo ipsum. Dolor duo eirmod sea justo no lorem est diam</p>
-                 <div class="w-100">
-                 <div class="input-group">
-                 <input type="text" class="form-control border-light" style="padding: 30px;" placeholder="Your Email Address">
-                 <div class="input-group-append">
-                 <button class="btn btn-primary px-4">Sign Up</button>
-                 </div>
-                 </div>
-                 </div>
-								 </div> -->
+<!-- Footer Start -->
+<div class="container-fluid bg-dark text-white py-5 px-sm-3 px-lg-5" style="margin-top: 90px;">
+  <div class="row pt-5">
+    <div class="col-12 mb-4 px-4">
+      
+			<div class="row mb-5 p-4" style="background: rgba(256, 256, 256, .05);">
+        <div class="col-md-4">
+          <div class="text-md-center">
+            <h5 class="text-primary text-uppercase mb-2" style="letter-spacing: 5px;">Nuestro taller</h5>
+						<p class="mb-4 m-md-0">Nueva San Martín 1096, Maipú</p>
+          </div>
         </div>
+        <div class="col-md-4">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3326.090134052772!2d-70.7741152849821!3d-33.52504210873008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662dd238b5f39d1%3A0x336c2e71bf198035!2sNueva%20San%20Mart%C3%ADn%201096%2C%20Maip%C3%BA%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1678127674036!5m2!1ses-419!2scl" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="col-md-4">
+          <div class="text-md-center">
+            <h5 class="text-primary text-uppercase mb-2" style="letter-spacing: 5px;">Llámanos</h5>
+            <p class="m-0">+569 5969 7897</p>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="container-fluid bg-dark text-white text-center border-top py-4 px-sm-3 px-md-5" style="border-color: rgba(256, 256, 256, .05) !important;">
-        <p class="m-0 text-white">&copy; <a href="#">Domain Name</a>. All Rights Reserved. Designed by <a href="https://htmlcodex.com">HTML Codex</a></p>
-    </div>
-    <!-- Footer End -->
+    <!-- <div class="col-lg-7 col-md-12">
+         <div class="row">
+         <div class="col-md-6 mb-5">
+         <p>Et et amet ut elitr ipsum sit dolor dolor dolore. Sit accusam elitr ut diam sit rebum. Eirmod duo kasd diam vero labore sea, est et et lorem ut at erat, gubergren ipsum et ipsum elitr et rebum rebum</p>
+         <div class="d-flex justify-content-start mt-4">
+         <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
+         <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
+         <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+         <a class="btn btn-lg btn-outline-light btn-lg-square" href="#"><i class="fab fa-instagram"></i></a>
+         </div>
+         </div>
+         <div class="col-md-6 mb-5">
+         <h5 class="text-primary text-uppercase mb-4" style="letter-spacing: 5px;">Our Products</h5>
+         <div class="d-flex flex-column justify-content-start">
+         <a class="text-white btn-scroll mb-2" href="#service"><i class="fa fa-angle-right mr-2"></i>Solar System</a>
+         <a class="text-white btn-scroll mb-2" href="#service"><i class="fa fa-angle-right mr-2"></i>Wind Turbine</a>
+         <a class="text-white btn-scroll mb-2" href="#service"><i class="fa fa-angle-right mr-2"></i>Wind Generator</a>
+         <a class="text-white btn-scroll mb-2" href="#service"><i class="fa fa-angle-right mr-2"></i>Solar Energy</a>
+         <a class="text-white btn-scroll" href="#service"><i class="fa fa-angle-right mr-2"></i>Solar Panel</a>
+         </div>
+         </div>
+         </div>
+				 </div>
+				 <div class="col-lg-5 col-md-12 mb-5">
+         <h5 class="text-primary text-uppercase mb-4" style="letter-spacing: 5px;">Newsletter</h5>
+         <p>Rebum labore lorem dolores kasd est, et ipsum amet et at kasd, ipsum sea tempor magna tempor. Accu kasd sed ea duo ipsum. Dolor duo eirmod sea justo no lorem est diam</p>
+         <div class="w-100">
+         <div class="input-group">
+         <input type="text" class="form-control border-light" style="padding: 30px;" placeholder="Your Email Address">
+         <div class="input-group-append">
+         <button class="btn btn-primary px-4">Sign Up</button>
+         </div>
+         </div>
+         </div>
+				 </div> -->
+  </div>
+</div>
+<div class="container-fluid bg-dark text-white text-center border-top py-4 px-sm-3 px-md-5" style="border-color: rgba(256, 256, 256, .05) !important;">
+  <p class="m-0 text-white">&copy; <a href="#">Paradis Service</a>. All Rights Reserved. Designed by <a target="_blank" href="https://htmlcodex.com">HTML Codex</a>. Implementado por <a href="https://cvillavicencio.com" target="_blank">cvillavicencio.com</a>.</p>
+</div>
+<!-- Footer End -->
